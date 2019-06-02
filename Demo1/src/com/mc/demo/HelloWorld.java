2f0c6d3652f90f6cba71af1d3c238e8be75cc3e2
865124492@qq.com <mocheng138>
2019-06-02 21:31:36 +0800

    添加个helloworld测试提交

--- Demo1/src/com/mc/demo/HelloWorld.java
+++ Demo1/src/com/mc/demo/HelloWorld.java
@@ -1,7 +1,16 @@
 package com.mc.demo;
 
+import java.util.HashMap;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("小明","一个人");
+        map.put("小伟","在这个夜里");
+        map.put("小王","寂寞到难以入睡");
+        System.out.println(map);
+
+
     }
 }
